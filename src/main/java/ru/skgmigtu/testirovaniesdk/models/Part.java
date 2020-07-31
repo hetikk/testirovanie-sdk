e3f3f9311f6893ad7efce4d8ebba43ad685c6efa
hetikk <h.kastuev99@inbox.ru>
2020-07-31 08:19:46 +0300

    добавил перечисление для уровней сложности тестирования

---
+++ src/main/java/ru/skgmigtu/testirovaniesdk/models/Part.java
@@ -0,0 +1,18 @@
+package ru.skgmigtu.testirovaniesdk.models;
+
+public enum Part {
+
+    A("1"),
+    B("2");
+
+    private String value;
+
+    Part(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+}
